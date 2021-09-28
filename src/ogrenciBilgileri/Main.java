c67ed06f4f961153c00bf96d0fe5086b9660588c
Berrkecan <berrkecantosun@gmail.com>
2021-09-28 14:05:08 +0300

    28092021

---
+++ src/ogrenciBilgileri/Main.java
@@ -0,0 +1,24 @@
+package ogrenciBilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("OGRENCI BILGILERI :");
+		System.out.print("Adýnýz Soyadýnýz:");
+		System.out.println("Berke Can Tosun");
+		System.out.print("");
+		System.out.print("Cinsiyetiniz:");
+		System.out.println("Eerkek");
+		System.out.println("Sýnýfýnýz ve Numaranýz:");
+		System.out.println("10/A,16547");
+		System.out.println("Doðum Yeri ve Doðum Tarihiniz:");
+		System.out.println("Bolu/Merkez,2006");
+		System.out.println("Ne yapmaktan hoþlanýrsýnýz?");
+		System.out.print("þiir yazmak,bazen ders calýsmak,yurt dýsý egitimi arastirmak, yabancý insanlar ile iletisim kurmaya calismak");
+		System.out.println("");
+		System.out.println("Ders dýþý faaliyetleriniz nelerdir?");
+		System.out.print("arkadasým ile sohbet etmek ve onunla gezmek,dil calismak,muzik dinlemek");
+	}
+
+}
